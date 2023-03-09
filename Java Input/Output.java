830d0fdfaa79d8f1d0db46da293918f9202273bd
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-03-09 14:41:54 +0530

    Input output

---
+++ Java Input/Output.java
@@ -0,0 +1,6 @@
+class Solution{
+    static void printIntString(String S,int N){
+        System.out.println("The input string :" + S);
+        System.out.println("The input integer :" + N);
+    }
+}
