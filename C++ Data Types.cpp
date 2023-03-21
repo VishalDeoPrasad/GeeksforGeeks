7c2aa3b1af6f6e3a4626a88dd8a697533606b19b
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-03-21 13:20:49 +0530

    C++ Data Types

---
+++ C++ Data Types.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int cppIntType() {
+        int n;
+        cin >> n;
+        return n;
+    }
+    
+    char cppCharType() {
+        char n;
+        cin >> n;
+        return n;
+    }
+    
+    float cppFloatType() {
+        float n;
+        cin >> n;
+        return n;
+    }
+};
