86d815d5f671c790f3d8176c7100337f947cfe3b
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-03-25 13:53:10 +0530

    Java Basic Data Types

---
+++ Java Basic Data Types.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    int javaIntType(Scanner sc) {
+        int a = sc.nextInt();
+        return a;
+    }
+    
+    String javaStringType(Scanner sc) {
+        String b = sc.next();
+        return b;
+    }
+    
+    float javaFloatType(Scanner sc) {
+        Float c = sc.nextFloat();
+        return c;
+    }
+};
