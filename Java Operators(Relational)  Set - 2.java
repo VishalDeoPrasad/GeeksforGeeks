85229a44080ca998558bd5c7a43fd81d6f1c1748
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-04-09 15:53:00 +0530

    java Operators set 2

---
+++ Java Operators(Relational)  Set - 2.java
@@ -0,0 +1,13 @@
+class Solution{
+    static void relationalOperators(int A,int B){
+        if (A>B){
+            System.out.println(A+" is greater than "+B);
+        }
+        else if (B>A){
+            System.out.println(A+" is less than "+B);
+        }
+        else{
+            System.out.println(A+" is equal to "+B);
+        }
+    }
+}
